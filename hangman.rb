105d0f88c881e4e52120d6ed22b311c8714dda42
Lance <dogmandevices@gmail.com>
2024-04-09 18:44:31 -0500

    wrap entire game in a module, create class, move dict definition into initialize

--- hangman.rb
+++ hangman.rb
@@ -1,4 +1,37 @@
-dict = File.read('google-10000-english-no-swears.txt').split.select do
-  |word| (word.length >= 5) && (word.length <= 12)
+module Hangman
+  class Game
+    MAX_TURNS = 11
+    attr_accessor :dict, :remaining_turns
+    def initialize
+      @dict = File.read('google-10000-english-no-swears.txt').split.select do
+        |word| (word.length >= 5) && (word.length <= 12)
+      end
+      @remaining_turns = MAX_TURNS
+    end
+
+    def play
+
+    end
+
+    def get_guess
+
+    end
+
+    def check_guess(g)
+
+    end
+
+    def check_word(w)
+
+    end
+
+    def update_gameboard
+
+    end
+  end
+
+    
 end
 
+include Hangman
+Game.new().play
